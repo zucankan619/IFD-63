3f5c966faa39d9d09528ab25ddcae1a51986f4f4
zucankan619 <96809336+zucankan619@users.noreply.github.com>
2022-01-06 10:39:04 +0600

    Add files via upload

---
+++ Lesson2.py
@@ -0,0 +1,26 @@
+x=20
+y=30
+
+z=x+y
+print("The Addition is : %s" % (z))
+
+z=x-y
+print("The Subtraction is : %s" % (z))
+
+z=x*y
+print("The Multiplication is : %s" % (z))
+
+z=x/y
+print("The Division is : %s" % (z))
+
+z=x**y
+print("The Exponent is : %s" % (z))
+
+z=x//y
+print("The Integar is : %s" % (z))
+
+z=x%y
+print("The Remainder is : %s" % (z))
+
+z=y%z
+print("The Remainder is : %s" % (z))
---
+++ lesson3.py
@@ -0,0 +1,5 @@
+firstName = input("Enter your First Name : ")
+lastName = input("Enter your Last Name : ")
+
+print("I am %s %s" % (lastName,firstName))
+print(f"I am {lastName} {firstName}")
