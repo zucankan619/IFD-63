31a04339433db0c5b788d01a0108d9460b975680
zucankan619 <96809336+zucankan619@users.noreply.github.com>
2022-01-13 13:10:01 +0600

    Fun Day 1 13-01-2022

---
+++ primefun.py
@@ -0,0 +1,26 @@
+from re import X
+
+
+n=11
+for i in range(2,n):
+    if n%i==0:
+        print(n, "is not a prime number")
+        break
+    else:
+        print(n, "is a prime number")
+        break
+# sum=0
+# data= [8,2,99,11,5]
+# for i in data:
+#     if i%2==0:
+#         sum= sum + x 
+
+# print(f"the sum is {sum}")
+
+#a =97 and z=122
+
+ for alpha in range(68,123):
+     print(chr(alpha,end=" "))
+
+for x in range (65,91):
+    print(chr(x,end=" "))     
