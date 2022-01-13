101bb12e3df9bd528f37a183423681ced26c6812
zucankan619 <96809336+zucankan619@users.noreply.github.com>
2022-01-13 12:45:18 +0600

    Fun Day 1 13-01-2022

--- forloopfun.py
+++ forloopfun.py
@@ -8,9 +8,16 @@ for  x in range(int(input("Value of x: "))):
 for  x in range(15,60,3):
     print(x,end='\t')
 
+sum=0
+data = [1,6,9,8,3]
+for  x in data:
+    sum= sum + x
+print(f"The sum is {sum}")
 
 data = [1,6,9,8,3]
 
 for  x in data:
     print(x,end='\n')
+
+    
 
