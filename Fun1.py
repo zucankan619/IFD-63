a275dff1362244755a7d3bcf175354c5df698376
zucankan619 <96809336+zucankan619@users.noreply.github.com>
2022-01-13 11:52:17 +0600

    Fun Day 1 13-01-2022

---
+++ Fun1.py
@@ -0,0 +1,11 @@
+from email.headerregistry import Address
+
+
+s=("Have fun with code \n\tHave fun with code \n\t\tHave fun with code \n\t\t\tHave fun with code \n\t\tHave fun with code \n\tHave fun with code \nHave fun with code")
+print(s)
+
+firstName = input("Enter 1st Name : ")
+Address = input("Enter Home : ")
+Age = input("Enter age : ")
+
+print(f"Information {firstName} {Address} {Age}")
---
+++ Fun2.py
@@ -0,0 +1,12 @@
+from tkinter import Y
+
+
+x=int(input("Enter value of x: "))
+y=int(input("Enter value of y: "))
+
+if x>y:
+    print("X is Big")
+elif x==y:
+     print("X equals to Y") 
+else: 
+     print("Y is Big")     
