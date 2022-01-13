e9f99d6c4985a8368f2318fb1f4158e2ae027af2
zucankan619 <96809336+zucankan619@users.noreply.github.com>
2022-01-13 12:35:08 +0600

    Fun Day 1 13-01-2022

--- forloopfun.py
+++ forloopfun.py
@@ -4,3 +4,13 @@ for variable in collection:
 """
 for  x in range(int(input("Value of x: "))):
     print(x,end='\n\n')
+
+for  x in range(15,60,3):
+    print(x,end='\t')
+
+
+data = [1,6,9,8,3]
+
+for  x in data:
+    print(x,end='\n')
+
